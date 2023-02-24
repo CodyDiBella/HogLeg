882fc7b0366d771265f552caffda1448299fbc11
Cody DiBella <codydibella@Codys-MacBook-Pro.local>
2023-02-24 11:23:02 -0600

    db.js fix

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,6 @@
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize')
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/juke', {
+  logging: false
+})
 
-const db = new Sequelize('deploy_test_lgkn', 'deploy_test_lgkn_user', 'DEsY8e9vVPtF1lxqgow4IHM0OhCLMbOQ', {
-  host: 'dpg-cfseaemn6mplpp8pl3b0-a.ohio-postgres.render.com',
-  dialect: 'postgres'
-});
-
-module.exports = db;
+module.exports = db
