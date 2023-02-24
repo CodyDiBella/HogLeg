66a89cd3ced55fa9abea7f17c1c18e4dec02fe4a
Cody DiBella <codydibella@Codys-MacBook-Pro.local>
2023-02-24 10:41:38 -0600

    .env

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "hogleg": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
