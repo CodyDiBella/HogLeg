a9f75aa81ce5ee384cb34adb3cfa1765461efe0d
Cody DiBella <codydibella@Codys-MacBook-Pro.local>
2023-02-24 11:08:24 -0600

    db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize('acme_schools_db', 'codydibella', null, {
-  host: 'localhost',
+const db = new Sequelize('deploy_test_lgkn', 'deploy_test_lgkn_user', 'DEsY8e9vVPtF1lxqgow4IHM0OhCLMbOQ', {
+  host: 'dpg-cfseaemn6mplpp8pl3b0-a.ohio-postgres.render.com',
   dialect: 'postgres'
 });
 
